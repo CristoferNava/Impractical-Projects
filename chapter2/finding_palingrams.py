2c1016f10f9184ee2e18d37f358b9429288ee622
CristoferNava <cristofernava11719@gmail.com>
2022-12-31 12:58:53 -0600

    finding palingrams was added

--- chapter2/finding_palingrams.py
+++ chapter2/finding_palingrams.py
@@ -1,16 +1,17 @@
 """Giving a file find all the words that are palindromes and returns a list of them."""
 from load_dictionary import load
 
+
 def check_palindrome(string: str) -> bool:
     """Given a string returns True if it is a palindrome otherwise returns False."""
     left, right = 0, len(string) - 1
 
     while left < right:
         if string[left] != string[right]:
-            return False 
+            return False
         left += 1
         right -= 1
-    
+
     return True
 
 
@@ -22,9 +23,8 @@ def main():
     for word in words:
         if check_palindrome(word):
             palindromes.append(word)
-    
+
     print(palindromes)
-     
 
 
 if __name__ == "__main__":
